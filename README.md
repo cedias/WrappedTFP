1722269277ec115d78a13a34e07b72c1958e9fe6
Charles-Emmanuel Dias <Charles-Emmanuel.Dias@etu.upmc.fr>
2018-02-07 16:15:31 +0100

    first mod

---
+++ README.md
@@ -0,0 +1,15 @@
+# WrappedTFP
+
+WrappedTFP is just Tensorflow Projector wrapped for local use:
+
+--------------------------------------
+
+## Install:
+- Clone & install with: `pip install -e .`
+
+## Usage:
+
+(Not functionnal yet.)
+
+`python -m wrappedtpf(port) (dir)`
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='WrappedTFP',
+      version='0.0',
+      description='wrapped Tensorflow Projector',
+      url='http://github.com/cedias/WrappedTFP',
+      author='cedias',
+      author_email='me@mymail.com',
+      license='MIT',
+      packages=['wrappedtfp'],
+      zip_safe=False)
