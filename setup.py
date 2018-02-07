848ab56e67a5aeca94a0a1db9b49d208b2474979
Charles-Emmanuel <Charles-Emmanuel.Dias@etu.upmc.fr>
2018-02-07 18:58:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='WrappedTFP',
-      version='0.0',
+      version='0.1',
       description='wrapped Tensorflow Projector',
       url='http://github.com/cedias/WrappedTFP',
       author='cedias',
